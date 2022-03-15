c9fe993d4a57f6b6f6b5362633cc73ae0ed20803
Igor Oliveira <igormat4us@gmail.com>
2022-03-15 20:58:11 -0300

    Adição de arquivo de Route eAdição de padrão mvc

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
-import express from 'express';
+import express, { request, response } from 'express';
+import { routes } from './config/routes';
 
 const app = express();
+app.use(routes);
 
 app.listen(934, () => {
     console.log("Servidor rodando!");
