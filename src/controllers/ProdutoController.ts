fc50f1fbaf357a7a15558eac1f5fa264c746d9ef
Igor Oliveira <igormat4us@gmail.com>
2022-03-15 21:30:57 -0300

    Adição de models, controller e padrão mvc

--- src/controllers/ProdutoController.ts
+++ src/controllers/ProdutoController.ts
@@ -1,11 +1,15 @@
+import { Produto } from './../models/Produto';
 import { Request } from 'express';
 import { Response } from 'express';
 
+const produtos : Produto[] = [];
+
 export class ProdutoController{
     create(request: Request, response: Response) {
+        produtos.push(request.body);
         response.
             status(201).
-            json({ message: "Produto cadastrado!" }) 
+            json({ message: "Produto cadastrado!", data: produtos }) 
     }
     list(request: Request, response: Response) {
         
